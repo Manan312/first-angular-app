d0cd9129f3a6274793293d4acbd537b870fa873d
Manan Garg <vrmanan@livainsurance.com>
2024-08-28 22:56:01 +0530

    Changes for changing from Components to Modules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+//Code for App Component
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+//Code for the App Module
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+platformBrowserDynamic().bootstrapModule(AppModule);
